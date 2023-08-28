7759bb791bf34f05429cae6d8388814eadb49a2d
Felipe C. Araujo <felipe.s-04@hotmail.com>
2023-08-27 22:59:10 -0300

    add assets

--- lib/src/barbershop_app.dart
+++ lib/src/barbershop_app.dart
@@ -1,5 +1,8 @@
+import 'package:asyncstate/widget/async_state_builder.dart';
 import 'package:barber_shop/src/features/splash/splash.dart';
 import 'package:flutter/material.dart';
+
+import './core/ui/widgets/widgets.dart';
 
 class BarberShopApp extends StatefulWidget {
   const BarberShopApp({super.key});
@@ -11,16 +14,22 @@ class BarberShopApp extends StatefulWidget {
 class _BarberShopAppState extends State<BarberShopApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'DW Barber Shop',
-      debugShowCheckedModeBanner: false,
-      routes: {
-        '/': (context) => const SplashPage(),
-      },
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-    );
+    return AsyncStateBuilder(
+        customLoader: const BarberShopLoader(),
+        enableLog: true,
+        builder: (asyncNavigatorObserver) {
+          return MaterialApp(
+            title: 'DW Barber Shop',
+            debugShowCheckedModeBanner: false,
+            routes: {
+              '/': (context) => const SplashPage(),
+            },
+            navigatorObservers: [asyncNavigatorObserver],
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+          );
+        });
   }
 }
