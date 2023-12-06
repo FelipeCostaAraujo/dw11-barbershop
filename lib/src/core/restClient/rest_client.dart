8b1f3f9a22da617e9ba1a3bc55213f8eb0aa4e97
Felipe C. Araujo <felipe.s-04@hotmail.com>
2023-12-06 14:35:08 -0300

    finish

--- lib/src/core/restClient/rest_client.dart
+++ lib/src/core/restClient/rest_client.dart
@@ -6,9 +6,7 @@ import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 final class RestClient extends DioForNative {
   RestClient()
       : super(BaseOptions(
-          baseUrl: 'http://192.168.1.88:8080',
-          //baseUrl: 'http://2804:d41:c624:2500:5c41:d59e:79f4:1496:8080',
-          //baseUrl: 'http://localhost:8080',
+          baseUrl: 'http://localhost:8080',
           connectTimeout: const Duration(seconds: 10),
           receiveTimeout: const Duration(seconds: 60),
         )) {
